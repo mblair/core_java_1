088e7def6d43e25cb2d249edfd8e19249b6c9a0a
Matt Blair <me@matthewblair.net>
2010-08-30 22:50:24 -0400

    Javadocs!

--- FirstSample.java
+++ FirstSample.java
@@ -1,3 +1,9 @@
+/**
+	This is the first sample program in Core Java Chapter 3
+	@version 1.0 2010-08-30
+	@author Matty B
+*/
+
 public class FirstSample {
 	public static void main(String[] args) {
 		System.out.println("We will not use 'Hello, World!'"); // Is this too cute?
