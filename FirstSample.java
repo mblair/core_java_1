859b66d9eb79fb65c1d931f2605003ccae6f72bf
Matt Blair <me@matthewblair.net>
2010-08-30 22:47:18 -0400

    Comment.

--- FirstSample.java
+++ FirstSample.java
@@ -1,5 +1,5 @@
 public class FirstSample {
 	public static void main(String[] args) {
-		System.out.println("We will not use 'Hello, World!'");
+		System.out.println("We will not use 'Hello, World!'"); // Is this too cute?
 	}
 }
