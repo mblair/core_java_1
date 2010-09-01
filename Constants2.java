415eee0a3ea344b4465f02d3aa452d6f9b60727c
Matt Blair <me@matthewblair.net>
2010-09-01 19:20:55 -0400

    More strictfp foolings.

--- Constants2.java
+++ Constants2.java
@@ -1,5 +1,5 @@
-public class Constants2 {
-	public static strictfp void main(String[] args) {
+public strictfp class Constants2 {
+	public static void main(String[] args) {
 		double paperWidth = 8.5;
 		double paperHeight = 11;
 		System.out.println("Paper size in centimeters: " + paperWidth * CM_PER_INCH + " by " + paperHeight * CM_PER_INCH);
