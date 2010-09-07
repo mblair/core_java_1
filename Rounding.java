cad94b0f146dd5667bcb09f6051dbc2f31b480c7
Matt Blair <me@matthewblair.net>
2010-09-06 22:40:17 -0400

    Rounding and casting.

---
+++ Rounding.java
@@ -0,0 +1,9 @@
+import static java.lang.Math.*;
+
+public class Rounding {
+	public static void main(String[] args) {
+		double pi = PI;
+		int int_pi = (int) Math.round(pi);
+		System.out.println(int_pi);
+	}
+}
