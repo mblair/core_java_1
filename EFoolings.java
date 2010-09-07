df157a00136cb5c50d74e8bf32b1581e234c5696
Matt Blair <me@matthewblair.net>
2010-09-06 20:20:52 -0400

    Messing with some more math constants and functions (E, pow & log)

---
+++ EFoolings.java
@@ -0,0 +1,8 @@
+import static java.lang.Math.*;
+
+public class EFoolings {
+	public static void main(String[] args) {
+		double x = pow(E, 10);
+		System.out.println(log(x)); // Should print 10.0.
+	}
+}
