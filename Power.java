9298c37534b1f451eff60cb50c25f649d651bd56
Matt Blair <me@matthewblair.net>
2010-09-06 20:17:19 -0400

    Import the math stuff instead.

--- Power.java
+++ Power.java
@@ -1,8 +1,10 @@
+import static java.lang.Math.*;
+
 public class Power {
 	public static void main(String[] args) {
 		double x = 9;
 		double y = 3;
-		double z = Math.pow(x, y);
-		System.out.println(z); // Should print 729.
+		double z = pow(x, y);
+		System.out.println(z); // Should print 729.0.
 	}
 }
