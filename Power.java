aad652af93fbb342a0697c4f291107550c79c3ae
Matt Blair <me@matthewblair.net>
2010-09-06 20:15:34 -0400

    Made Power use two vars.

--- Power.java
+++ Power.java
@@ -1,7 +1,8 @@
 public class Power {
 	public static void main(String[] args) {
 		double x = 9;
-		double y = Math.pow(x, 2);
-		System.out.println(y); // Should print 81.
+		double y = 3;
+		double z = Math.pow(x, y);
+		System.out.println(z); // Should print 729.
 	}
 }
